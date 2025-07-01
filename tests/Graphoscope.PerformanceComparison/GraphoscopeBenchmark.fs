468804bd2b3bfc0f71c611f27c9d5ef0ced09c74
Oliver Maus <oliver.maus@eclipso.de>
2025-07-01 11:06:04 +0200

    Add 2000 Nodes, 2700 Edges graphs to comparison benchmarks

--- tests/Graphoscope.PerformanceComparison/GraphoscopeBenchmark.fs
+++ tests/Graphoscope.PerformanceComparison/GraphoscopeBenchmark.fs
@@ -10,18 +10,6 @@ open Graphoscope.PerformanceComparison.Fixtures.Graphoscope
 [<MemoryDiagnoser>]
 type GraphoscopeBenchmark() =
 
-    //[<GlobalSetup>]
-    //member _.SetupSmallGraph() =
-    //    FGraphs.n20e27
-
-    //[<GlobalSetup>]
-    //member _.SetupMiddleGraph() =
-    //    FGraphs.n200e270
-
-    //[<GlobalSetup>]
-    //member _.SetupBigGraph() =
-    //    FGraphs.n2000e2700
-
     [<Benchmark>]
     member _.fgn20e27DFS1000Times() =
         let nk1 = FGraph.getNodes FGraphs.n20e27 |> Seq.head |> fst
@@ -34,11 +22,11 @@ type GraphoscopeBenchmark() =
         for i = 1 to 1000 do
             DFS.ofFGraph nk1 FGraphs.n200e270 |> ignore
 
-    //[<Benchmark>]
-    //member _.fgn2000e2700DFS1000Times() =
-    //    let nk1 = FGraph.getNodes FGraphs.n2000e2700 |> Seq.head |> fst
-    //    for i = 1 to 1000 do
-    //        DFS.ofFGraph nk1 FGraphs.n2000e2700 |> ignore
+    [<Benchmark>]
+    member _.fgn2000e2700DFS1000Times() =
+        let nk1 = FGraph.getNodes FGraphs.n2000e2700 |> Seq.head |> fst
+        for i = 1 to 1000 do
+            DFS.ofFGraph nk1 FGraphs.n2000e2700 |> ignore
 
 
     [<Benchmark>]
@@ -53,11 +41,11 @@ type GraphoscopeBenchmark() =
         for i = 1 to 1000 do
             BFS.ofFGraph nk1 FGraphs.n200e270 |> ignore
 
-    //[<Benchmark>]
-    //member _.fgn2000e2700BFS1000Times() =
-    //    let nk1 = FGraph.getNodes FGraphs.n2000e2700 |> Seq.head |> fst
-    //    for i = 1 to 1000 do
-    //        BFS.ofFGraph nk1 FGraphs.n2000e2700 |> ignore
+    [<Benchmark>]
+    member _.fgn2000e2700BFS1000Times() =
+        let nk1 = FGraph.getNodes FGraphs.n2000e2700 |> Seq.head |> fst
+        for i = 1 to 1000 do
+            BFS.ofFGraph nk1 FGraphs.n2000e2700 |> ignore
 
 
 
@@ -73,8 +61,8 @@ type GraphoscopeBenchmark() =
         for i = 1 to 1000 do
             DFS.ofDiGraph nk1 DiGraphs.n200e270 |> ignore
 
-    //[<Benchmark>]
-    //member _.dgn2000e2700DFS1000Times() =
-    //    let nk1 = DiGraph.getNodes DiGraphs.n2000e2700 |> Seq.head
-    //    for i = 1 to 1000 do
-    //        DFS.ofDiGraph nk1 DiGraphs.n2000e2700 |> ignore
+    [<Benchmark>]
+    member _.dgn2000e2700DFS1000Times() =
+        let nk1 = DiGraph.getNodes DiGraphs.n2000e2700 |> Seq.head
+        for i = 1 to 1000 do
+            DFS.ofDiGraph nk1 DiGraphs.n2000e2700 |> ignore
--- tests/Graphoscope.PerformanceComparison/QuikGraphBenchmark.fs
+++ tests/Graphoscope.PerformanceComparison/QuikGraphBenchmark.fs
@@ -18,10 +18,10 @@ type QuikGraphBenchmark() =
         for i = 1 to 1000 do
             AdjacencyGraph.computeDFS (fun _ -> ()) 1 AdjGraphs.n200e270
 
-    //[<Benchmark>]
-    //member _.agn2000e2700DFS1000Times() =
-    //    for i = 1 to 1000 do
-    //        AdjacencyGraph.computeDFS (fun _ -> ()) 1 AdjGraphs.n2000e2700
+    [<Benchmark>]
+    member _.agn2000e2700DFS1000Times() =
+        for i = 1 to 1000 do
+            AdjacencyGraph.computeDFS (fun _ -> ()) 1 AdjGraphs.n2000e2700
 
 
     [<Benchmark>]
@@ -34,7 +34,7 @@ type QuikGraphBenchmark() =
         for i = 1 to 1000 do
             AdjacencyGraph.computeBFS (fun _ -> ()) 1 AdjGraphs.n200e270
 
-    //[<Benchmark>]
-    //member _.agn2000e2700BFS1000Times() =
-    //    for i = 1 to 1000 do
-    //        AdjacencyGraph.computeBFS (fun _ -> ()) 1 AdjGraphs.n2000e2700
+    [<Benchmark>]
+    member _.agn2000e2700BFS1000Times() =
+        for i = 1 to 1000 do
+            AdjacencyGraph.computeBFS (fun _ -> ()) 1 AdjGraphs.n2000e2700

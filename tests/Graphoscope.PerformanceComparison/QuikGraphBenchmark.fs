811f47c5a7d2b4c9bc2fc32d91e244616315f8ff
Oliver Maus <oliver.maus@eclipso.de>
2025-07-01 20:17:02 +0200

    Add more comparison benchmarks

--- tests/Graphoscope.PerformanceComparison/QuikGraphBenchmark.fs
+++ tests/Graphoscope.PerformanceComparison/QuikGraphBenchmark.fs
@@ -9,32 +9,52 @@ open Graphoscope.PerformanceComparison.Fixtures.QuikGraph
 type QuikGraphBenchmark() =
 
     [<Benchmark>]
-    member _.agn20e27DFS10000Times() =
-        for i = 1 to 10000 do
+    member _.agn20e27DFS1000Times() =
+        for i = 1 to 1000 do
             AdjacencyGraph.computeDFS (fun _ -> ()) 1 AdjGraphs.n20e27
 
     [<Benchmark>]
-    member _.agn200e270DFS10000Times() =
-        for i = 1 to 10000 do
+    member _.agn200e270DFS1000Times() =
+        for i = 1 to 1000 do
             AdjacencyGraph.computeDFS (fun _ -> ()) 1 AdjGraphs.n200e270
 
     [<Benchmark>]
-    member _.agn2000e2700DFS10000Times() =
-        for i = 1 to 10000 do
+    member _.agn2000e2700DFS1000Times() =
+        for i = 1 to 1000 do
             AdjacencyGraph.computeDFS (fun _ -> ()) 1 AdjGraphs.n2000e2700
+
+    [<Benchmark>]
+    member _.agn20000e27000DFS1000Times() =
+        for i = 1 to 1000 do
+            AdjacencyGraph.computeDFS (fun _ -> ()) 1 AdjGraphs.n20000e27000
+
+    [<Benchmark>]
+    member _.agn200000e270000DFS1000Times() =
+        for i = 1 to 1000 do
+            AdjacencyGraph.computeDFS (fun _ -> ()) 1 AdjGraphs.n200000e270000
 
 
     [<Benchmark>]
-    member _.agn20e27BFS10000Times() =
-        for i = 1 to 10000 do
+    member _.agn20e27BFS1000Times() =
+        for i = 1 to 1000 do
             AdjacencyGraph.computeBFS (fun _ -> ()) 1 AdjGraphs.n20e27
 
     [<Benchmark>]
-    member _.agn200e270BFS10000Times() =
-        for i = 1 to 10000 do
+    member _.agn200e270BFS1000Times() =
+        for i = 1 to 1000 do
             AdjacencyGraph.computeBFS (fun _ -> ()) 1 AdjGraphs.n200e270
 
     [<Benchmark>]
-    member _.agn2000e2700BFS10000Times() =
-        for i = 1 to 10000 do
+    member _.agn2000e2700BFS1000Times() =
+        for i = 1 to 1000 do
             AdjacencyGraph.computeBFS (fun _ -> ()) 1 AdjGraphs.n2000e2700
+
+    [<Benchmark>]
+    member _.agn20000e27000BFS1000Times() =
+        for i = 1 to 1000 do
+            AdjacencyGraph.computeBFS (fun _ -> ()) 1 AdjGraphs.n20000e27000
+
+    [<Benchmark>]
+    member _.agn200000e270000BFS1000Times() =
+        for i = 1 to 1000 do
+            AdjacencyGraph.computeBFS (fun _ -> ()) 1 AdjGraphs.n200000e270000

fc1075b153d24f3beaf81c6d8484208a66c89098
Oliver Maus <oliver.maus@eclipso.de>
2025-07-01 19:27:32 +0200

    Raise comparisal benchmark repetitions to 10 000

--- tests/Graphoscope.PerformanceComparison/GraphoscopeBenchmark.fs
+++ tests/Graphoscope.PerformanceComparison/GraphoscopeBenchmark.fs
@@ -11,58 +11,58 @@ open Graphoscope.PerformanceComparison.Fixtures.Graphoscope
 type GraphoscopeBenchmark() =
 
     [<Benchmark>]
-    member _.fgn20e27DFS1000Times() =
+    member _.fgn20e27DFS10000Times() =
         let nk1 = FGraph.getNodes FGraphs.n20e27 |> Seq.head |> fst
-        for i = 1 to 1000 do
+        for i = 1 to 10000 do
             DFS.ofFGraph nk1 FGraphs.n20e27 |> ignore
 
     [<Benchmark>]
-    member _.fgn200e270DFS1000Times() =
+    member _.fgn200e270DFS10000Times() =
         let nk1 = FGraph.getNodes FGraphs.n200e270 |> Seq.head |> fst
-        for i = 1 to 1000 do
+        for i = 1 to 10000 do
             DFS.ofFGraph nk1 FGraphs.n200e270 |> ignore
 
     [<Benchmark>]
-    member _.fgn2000e2700DFS1000Times() =
+    member _.fgn2000e2700DFS10000Times() =
         let nk1 = FGraph.getNodes FGraphs.n2000e2700 |> Seq.head |> fst
-        for i = 1 to 1000 do
+        for i = 1 to 10000 do
             DFS.ofFGraph nk1 FGraphs.n2000e2700 |> ignore
 
 
     [<Benchmark>]
-    member _.fgn20e27BFS1000Times() =
+    member _.fgn20e27BFS10000Times() =
         let nk1 = FGraph.getNodes FGraphs.n20e27 |> Seq.head |> fst
-        for i = 1 to 1000 do
+        for i = 1 to 10000 do
             BFS.ofFGraph nk1 FGraphs.n20e27 |> ignore
 
     [<Benchmark>]
-    member _.fgn200e270BFS1000Times() =
+    member _.fgn200e270BFS10000Times() =
         let nk1 = FGraph.getNodes FGraphs.n200e270 |> Seq.head |> fst
-        for i = 1 to 1000 do
+        for i = 1 to 10000 do
             BFS.ofFGraph nk1 FGraphs.n200e270 |> ignore
 
     [<Benchmark>]
-    member _.fgn2000e2700BFS1000Times() =
+    member _.fgn2000e2700BFS10000Times() =
         let nk1 = FGraph.getNodes FGraphs.n2000e2700 |> Seq.head |> fst
-        for i = 1 to 1000 do
+        for i = 1 to 10000 do
             BFS.ofFGraph nk1 FGraphs.n2000e2700 |> ignore
 
 
 
     [<Benchmark>]
-    member _.dgn20e27DFS1000Times() =
+    member _.dgn20e27DFS10000Times() =
         let nk1 = DiGraph.getNodes DiGraphs.n20e27 |> Seq.head
-        for i = 1 to 1000 do
+        for i = 1 to 10000 do
             DFS.ofDiGraph nk1 DiGraphs.n20e27 |> ignore
 
     [<Benchmark>]
-    member _.dgn200e270DFS1000Times() =
+    member _.dgn200e270DFS10000Times() =
         let nk1 = DiGraph.getNodes DiGraphs.n200e270 |> Seq.head
-        for i = 1 to 1000 do
+        for i = 1 to 10000 do
             DFS.ofDiGraph nk1 DiGraphs.n200e270 |> ignore
 
     [<Benchmark>]
-    member _.dgn2000e2700DFS1000Times() =
+    member _.dgn2000e2700DFS10000Times() =
         let nk1 = DiGraph.getNodes DiGraphs.n2000e2700 |> Seq.head
-        for i = 1 to 1000 do
+        for i = 1 to 10000 do
             DFS.ofDiGraph nk1 DiGraphs.n2000e2700 |> ignore
--- tests/Graphoscope.PerformanceComparison/QuikGraphBenchmark.fs
+++ tests/Graphoscope.PerformanceComparison/QuikGraphBenchmark.fs
@@ -9,32 +9,32 @@ open Graphoscope.PerformanceComparison.Fixtures.QuikGraph
 type QuikGraphBenchmark() =
 
     [<Benchmark>]
-    member _.agn20e27DFS1000Times() =
-        for i = 1 to 1000 do
+    member _.agn20e27DFS10000Times() =
+        for i = 1 to 10000 do
             AdjacencyGraph.computeDFS (fun _ -> ()) 1 AdjGraphs.n20e27
 
     [<Benchmark>]
-    member _.agn200e270DFS1000Times() =
-        for i = 1 to 1000 do
+    member _.agn200e270DFS10000Times() =
+        for i = 1 to 10000 do
             AdjacencyGraph.computeDFS (fun _ -> ()) 1 AdjGraphs.n200e270
 
     [<Benchmark>]
-    member _.agn2000e2700DFS1000Times() =
-        for i = 1 to 1000 do
+    member _.agn2000e2700DFS10000Times() =
+        for i = 1 to 10000 do
             AdjacencyGraph.computeDFS (fun _ -> ()) 1 AdjGraphs.n2000e2700
 
 
     [<Benchmark>]
-    member _.agn20e27BFS1000Times() =
-        for i = 1 to 1000 do
+    member _.agn20e27BFS10000Times() =
+        for i = 1 to 10000 do
             AdjacencyGraph.computeBFS (fun _ -> ()) 1 AdjGraphs.n20e27
 
     [<Benchmark>]
-    member _.agn200e270BFS1000Times() =
-        for i = 1 to 1000 do
+    member _.agn200e270BFS10000Times() =
+        for i = 1 to 10000 do
             AdjacencyGraph.computeBFS (fun _ -> ()) 1 AdjGraphs.n200e270
 
     [<Benchmark>]
-    member _.agn2000e2700BFS1000Times() =
-        for i = 1 to 1000 do
+    member _.agn2000e2700BFS10000Times() =
+        for i = 1 to 10000 do
             AdjacencyGraph.computeBFS (fun _ -> ()) 1 AdjGraphs.n2000e2700

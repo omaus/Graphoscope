0cd9bd230adf7008aaef814ed63f6d316540a58c
Oliver Maus <oliver.maus@eclipso.de>
2025-07-01 19:57:36 +0200

    Fix Graphoscope benchmark bug

--- tests/Graphoscope.PerformanceComparison/GraphoscopeBenchmark.fs
+++ tests/Graphoscope.PerformanceComparison/GraphoscopeBenchmark.fs
@@ -14,38 +14,38 @@ type GraphoscopeBenchmark() =
     member _.fgn20e27DFS10000Times() =
         let nk1 = FGraph.getNodes FGraphs.n20e27 |> Seq.head |> fst
         for i = 1 to 10000 do
-            DFS.ofFGraph nk1 FGraphs.n20e27 |> ignore
+            DFS.ofFGraph nk1 FGraphs.n20e27 |> Seq.length |> ignore
 
     [<Benchmark>]
     member _.fgn200e270DFS10000Times() =
         let nk1 = FGraph.getNodes FGraphs.n200e270 |> Seq.head |> fst
         for i = 1 to 10000 do
-            DFS.ofFGraph nk1 FGraphs.n200e270 |> ignore
+            DFS.ofFGraph nk1 FGraphs.n200e270 |> Seq.length |> ignore
 
     [<Benchmark>]
     member _.fgn2000e2700DFS10000Times() =
         let nk1 = FGraph.getNodes FGraphs.n2000e2700 |> Seq.head |> fst
         for i = 1 to 10000 do
-            DFS.ofFGraph nk1 FGraphs.n2000e2700 |> ignore
+            DFS.ofFGraph nk1 FGraphs.n2000e2700 |> Seq.length |> ignore
 
 
     [<Benchmark>]
     member _.fgn20e27BFS10000Times() =
         let nk1 = FGraph.getNodes FGraphs.n20e27 |> Seq.head |> fst
         for i = 1 to 10000 do
-            BFS.ofFGraph nk1 FGraphs.n20e27 |> ignore
+            BFS.ofFGraph nk1 FGraphs.n20e27 |> Seq.length |> ignore
 
     [<Benchmark>]
     member _.fgn200e270BFS10000Times() =
         let nk1 = FGraph.getNodes FGraphs.n200e270 |> Seq.head |> fst
         for i = 1 to 10000 do
-            BFS.ofFGraph nk1 FGraphs.n200e270 |> ignore
+            BFS.ofFGraph nk1 FGraphs.n200e270 |> Seq.length |> ignore
 
     [<Benchmark>]
     member _.fgn2000e2700BFS10000Times() =
         let nk1 = FGraph.getNodes FGraphs.n2000e2700 |> Seq.head |> fst
         for i = 1 to 10000 do
-            BFS.ofFGraph nk1 FGraphs.n2000e2700 |> ignore
+            BFS.ofFGraph nk1 FGraphs.n2000e2700 |> Seq.length |> ignore
 
 
 
@@ -53,16 +53,16 @@ type GraphoscopeBenchmark() =
     member _.dgn20e27DFS10000Times() =
         let nk1 = DiGraph.getNodes DiGraphs.n20e27 |> Seq.head
         for i = 1 to 10000 do
-            DFS.ofDiGraph nk1 DiGraphs.n20e27 |> ignore
+            DFS.ofDiGraph nk1 DiGraphs.n20e27 |> Seq.length |> ignore
 
     [<Benchmark>]
     member _.dgn200e270DFS10000Times() =
         let nk1 = DiGraph.getNodes DiGraphs.n200e270 |> Seq.head
         for i = 1 to 10000 do
-            DFS.ofDiGraph nk1 DiGraphs.n200e270 |> ignore
+            DFS.ofDiGraph nk1 DiGraphs.n200e270 |> Seq.length |> ignore
 
     [<Benchmark>]
     member _.dgn2000e2700DFS10000Times() =
         let nk1 = DiGraph.getNodes DiGraphs.n2000e2700 |> Seq.head
         for i = 1 to 10000 do
-            DFS.ofDiGraph nk1 DiGraphs.n2000e2700 |> ignore
+            DFS.ofDiGraph nk1 DiGraphs.n2000e2700 |> Seq.length |> ignore
